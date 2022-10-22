15e18c9ec4503d6a6ce131a8e58fafee6c2dd085
redhat-debug <https://github.com/redhat-debug/ishq.git>
2022-10-22 18:57:31 +0600

    vercel added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
    res.send('Welcome')
@@ -13,6 +14,26 @@ app.get('/', (req, res) => {
 app.get('/news-categories', (req, res) => {
    res.send(categories);
 });
+
+app.get('/category/:id', (req, res) => {
+   const id = req.params.id;
+   if (id === '08') {
+      res.send(news);
+   } else {
+      const category_news = news.filter(n => n.category_id === id);
+      res.send(category_news);
+   }
+})
+
+app.get('/news', (req, res) => {
+   res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+   const id = req.params.id;
+   const selectedNews = news.find(news => news._id === id);
+   res.send(selectedNews);
+})
 
 app.listen(port, () => {
    console.log('Dragon news Running on port', port);
